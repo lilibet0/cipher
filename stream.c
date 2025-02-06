30e837c6a817f67ecb7d91d1ea09c872da84f1d4
Elisabet Clay <157527637+lilibet0@users.noreply.github.com>
2025-02-06 14:30:34 -0500

    Stream cipher

--- stream.c
+++ stream.c
@@ -7,6 +7,27 @@ extern void stream_cipher(char *args[]) {
     FILE *output_file = fopen(args[3], "w");
     FILE *key_file = fopen(args[4], "r");
 
-    printf("dreamy streamy!!\n");
+    // Get key from keyfile
+
+    // Find length of key
+    fseek(key_file, 0, SEEK_END);
+    long key_size = ftell(key_file);
+    fseek(key_file, 0, SEEK_SET);
+    
+    // Store key
+    char key[key_size];
+    fgets(key, key_size + 1, key_file);
+
+    // XOR
+    char ch;
+    char output;
+    int current_key_index = 0;
+
+    while((ch = fgetc(input_file)) != EOF) {
+        output = (char)ch^(key[current_key_index % key_size]);
+        fprintf(output_file, "%c", output);
+        current_key_index++;
+    }
+
     exit(0);
 }
