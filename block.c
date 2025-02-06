a673c7e57ec1d8b0a3cb9b240fe91efea3603e5f
Elisabet Clay <157527637+lilibet0@users.noreply.github.com>
2025-02-06 14:51:37 -0500

    Block cipher method, add empty fn for encryption and decryption

--- block.c
+++ block.c
@@ -1,12 +1,36 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-extern void block_cipher(char *args[]) {
+void encrypt(FILE *input_file, FILE *output_file, char key[])
+{
+    printf("encrypting!\n");
+}
+
+void decrypt(FILE *input_file, FILE *output_file, char key[])
+{
+    printf("decrypting!\n");
+}
+
+void block_cipher(char *args[])
+{
     // Open files
     FILE *input_file = fopen(args[2], "r");
     FILE *output_file = fopen(args[3], "w");
     FILE *key_file = fopen(args[4], "r");
 
-    printf("blockity block!!\n");
+    // Get key
+    // Key file will contain 16 characters for the block cipher
+    char key[16];
+    fgets(key, 16 + 1, key_file);
+
+    if (*args[5] == 'E')
+    {
+        encrypt(input_file, output_file, key);
+    }
+
+    else {
+        decrypt(input_file, output_file, key);
+    }
+    
     exit(0);
 }
